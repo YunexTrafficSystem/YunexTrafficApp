108d09b10643fa938cb34d9864457f5e19b064be
Katerine  Ospina <ospinakaterine4@gmail.com>
2022-08-23 12:49:12 -0500

    Correcciones mínimas en ignore

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,13 @@ import ReactDOM from 'react-dom/client'
 import Registro from './Registro'
 import Nav from './Nav'
 import Footer from './Footer'
-import { ThemeProvider } from '@mui/material'
+import { createTheme } from '@mui/material/styles'
+
+const theme = createTheme({
+  palette: {
+    prim
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
